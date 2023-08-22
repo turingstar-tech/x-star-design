ec2bce596fda21ddc8e037fa80703e30307dcb1d
zhangjiajun <183728895@qq.com>
2023-08-22 15:41:11 +0800

    feat: error-boundary完成

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 export { default as CodeMirrorWrapper } from './code-mirror-wrapper';
 export { LangId, Language, Theme } from './code-mirror-wrapper/define';
+export { default as ErrorBoundary } from './error-boundary';
+export type { FallbackProps } from './error-boundary';
 export { default as StatusTag } from './status-tag';
 export type {
   StatusTagProps,

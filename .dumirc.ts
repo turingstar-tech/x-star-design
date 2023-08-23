e07425b22b57bdb5cfbef77f880cda155e57a8dc
zhangjiajun <183728895@qq.com>
2023-08-23 16:56:23 +0800

    fix: dumi public path config

--- .dumirc.ts
+++ .dumirc.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'dumi';
 
 export default defineConfig({
   base: '/x-star-design/',
+  publicPath: '/x-star-design/',
   outputPath: 'docs-dist',
   themeConfig: {
     name: 'x-star-design',

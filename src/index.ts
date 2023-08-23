6d036185b63baf43f21aa7f006020dd63afe0bbb
zhangjiajun <183728895@qq.com>
2023-08-23 11:05:46 +0800

    feat: 完成LoadingMask组件

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export { LangId, Language, Theme } from './code-mirror-wrapper/define';
 export { default as DraggableLayout } from './draggable-layout';
 export { default as ErrorBoundary } from './error-boundary';
 export type { FallbackProps } from './error-boundary';
+export { default as LoadingMask } from './loading-mask';
 export { default as RainbowCat } from './rainbow-cat';
 export { default as StatusTag } from './status-tag';
 export type {
---
+++ src/loading-mask/index.md
@@ -0,0 +1,17 @@
+# LoadingMask
+
+这是 Loading 组件，常用于页面、组件加载
+
+```jsx
+import { LoadingMask } from 'x-star-design';
+
+export default () => (
+  <div style={{ position: 'relative', height: 500 }}>
+    <LoadingMask loading={true} />
+  </div>
+);
+```
+
+## API
+
+<API id="LoadingMask"></API>

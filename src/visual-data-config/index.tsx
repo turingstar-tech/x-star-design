b9d450f3c30bae65f8da395f741d80d2bc0de8aa
zhangjiajun <183728895@qq.com>
2023-09-28 13:46:02 +0800

    fix: 修复组件样式

--- src/visual-data-config/index.tsx
+++ src/visual-data-config/index.tsx
@@ -9,6 +9,7 @@ import {
   Row,
 } from 'antd';
 import React, { useEffect } from 'react';
+import ConfigProviderWrapper from '../config-provider-wrapper';
 import { useLocale } from '../locales';
 import { prefix } from '../utils/global';
 import SingleDataConfig from './SingleDataConfig';
@@ -70,134 +71,139 @@ const VisualDataConfig = ({
     };
   };
   return (
-    <Form
-      form={form}
-      onFinish={async (values) => {
-        const config = configTranslate(values);
-        const res = compactObj(config); // 删除空对象
-        onConfirm(JSON.stringify(res));
-      }}
-      className={`${prefix}form`}
-    >
-      <Row justify="start" gutter={[16, 0]} className={`${prefix}row`}>
-        <Col span={6}>
-          <Form.Item
-            name="timeLimit"
-            label={t('Time_MS')}
-            rules={[{ required: true }]}
-          >
-            <InputNumber />
-          </Form.Item>
-        </Col>
-        <Col span={6}>
-          <Form.Item
-            name="memoryLimit"
-            label={t('Space_MS')}
-            rules={[{ required: true }]}
-          >
-            <InputNumber />
-          </Form.Item>
-        </Col>
-        <Col span={24}>
-          <Form.Item
-            name="points"
-            label={t('Individual_Assessment')}
-            extra={t('Individual_Assessment_Tip')}
-          >
-            <InputNumber />
-          </Form.Item>
-        </Col>
-        <Col span={24}>
-          <Form.Item
-            name="checkTarget"
-            label={t('SPJ_EXE_FILE')}
-            extra={t('SPJ_EXE_FILE_TIP')}
-          >
-            <Input style={{ width: 200 }} />
-          </Form.Item>
-        </Col>
-        <Col span={8}>
-          <Form.Item
-            name="inputFormat"
-            label={t('Input_Format')}
-            rules={[{ required: true }]}
-          >
-            <Input />
-          </Form.Item>
-        </Col>
-        <Col span={9}>
-          <Form.Item
-            name="outputFormat"
-            label={t('Output_Format')}
-            rules={[{ required: true }]}
-          >
-            <Input />
-          </Form.Item>
-        </Col>
-        <Col span={24}>
-          <div className={`ant-form-item-extra ${`${prefix}explainItem`}`}>
-            {t('Input_Output_Format_Tip')}
-          </div>
-        </Col>
-        <Col span={8}>
-          <Form.Item name="fileInputFormat" label={t('File_Input_Format')}>
-            <Input />
-          </Form.Item>
-        </Col>
-
-        <Col span={8}>
-          <Form.Item name="fileOutputFormat" label={t('File_Output_Format')}>
-            <Input />
-          </Form.Item>
-        </Col>
-        <Col span={24}>
-          <div className={`ant-form-item-extra ${prefix}explainItem`}>
-            {t('File_Input_Output_Format_Tip')}
-          </div>
-        </Col>
-        <Col span={12}>
-          <Form.Item
-            name="buildInput"
-            label={t('Interactive_Library')}
-            extra={t('Interactive_Library_Tip')}
-          >
-            <Input />
-          </Form.Item>
-        </Col>
-        <Col span={24}>
-          <Form.Item
-            name="judgeWay"
-            initialValue={'single'}
-            label={t('Evaluation_Method')}
-            rules={[{ required: true }]}
-          >
-            <Radio.Group>
-              <Radio value={'single'}>{t('Single_Point_Data_Testing')}</Radio>
-              <Radio value={'subtask'}>{t('SubTask')}</Radio>
-            </Radio.Group>
-          </Form.Item>
-        </Col>
-      </Row>
-      <Form.Item
-        noStyle
-        shouldUpdate={(perValues, nextValues) => {
-          return perValues['judgeWay'] !== nextValues['judgeWay'];
+    <ConfigProviderWrapper>
+      <Form
+        form={form}
+        onFinish={async (values) => {
+          const config = configTranslate(values);
+          const res = compactObj(config); // 删除空对象
+          onConfirm(JSON.stringify(res));
         }}
+        className={`${prefix}form`}
       >
-        {({ getFieldValue }) => {
-          const isVisible =
-            getFieldValue('judgeWay') && getFieldValue('judgeWay') === 'single';
+        <Row justify="start" gutter={[16, 0]} className={`${prefix}row`}>
+          <Col span={6}>
+            <Form.Item
+              name="timeLimit"
+              label={t('Time_MS')}
+              rules={[{ required: true }]}
+            >
+              <InputNumber />
+            </Form.Item>
+          </Col>
+          <Col span={6}>
+            <Form.Item
+              name="memoryLimit"
+              label={t('Space_MS')}
+              rules={[{ required: true }]}
+            >
+              <InputNumber />
+            </Form.Item>
+          </Col>
+          <Col span={24}>
+            <Form.Item
+              name="points"
+              label={t('Individual_Assessment')}
+              extra={t('Individual_Assessment_Tip')}
+            >
+              <InputNumber />
+            </Form.Item>
+          </Col>
+          <Col span={24}>
+            <Form.Item
+              name="checkTarget"
+              label={t('SPJ_EXE_FILE')}
+              extra={t('SPJ_EXE_FILE_TIP')}
+            >
+              <Input style={{ width: 200 }} />
+            </Form.Item>
+          </Col>
+          <Col span={8}>
+            <Form.Item
+              name="inputFormat"
+              label={t('Input_Format')}
+              rules={[{ required: true }]}
+            >
+              <Input />
+            </Form.Item>
+          </Col>
+          <Col span={9}>
+            <Form.Item
+              name="outputFormat"
+              label={t('Output_Format')}
+              rules={[{ required: true }]}
+            >
+              <Input />
+            </Form.Item>
+          </Col>
+          <Col span={24}>
+            <div
+              className={`${prefix}form-item-extra ${`${prefix}explainItem`}`}
+            >
+              {t('Input_Output_Format_Tip')}
+            </div>
+          </Col>
+          <Col span={8}>
+            <Form.Item name="fileInputFormat" label={t('File_Input_Format')}>
+              <Input />
+            </Form.Item>
+          </Col>
 
-          return <>{isVisible ? <SingleDataConfig /> : <SubTaskConfig />}</>;
-        }}
-      </Form.Item>
-      <Divider />
-      <Form.Item>
-        <Button type={'primary'} htmlType="submit">
-          {t('Confirm_Import')}
-        </Button>
-      </Form.Item>
-    </Form>
+          <Col span={8}>
+            <Form.Item name="fileOutputFormat" label={t('File_Output_Format')}>
+              <Input />
+            </Form.Item>
+          </Col>
+          <Col span={24}>
+            <div className={`${prefix}form-item-extra ${prefix}explainItem`}>
+              {t('File_Input_Output_Format_Tip')}
+            </div>
+          </Col>
+          <Col span={12}>
+            <Form.Item
+              name="buildInput"
+              label={t('Interactive_Library')}
+              extra={t('Interactive_Library_Tip')}
+            >
+              <Input />
+            </Form.Item>
+          </Col>
+          <Col span={24}>
+            <Form.Item
+              name="judgeWay"
+              initialValue={'single'}
+              label={t('Evaluation_Method')}
+              rules={[{ required: true }]}
+            >
+              <Radio.Group>
+                <Radio value={'single'}>{t('Single_Point_Data_Testing')}</Radio>
+                <Radio value={'subtask'}>{t('SubTask')}</Radio>
+              </Radio.Group>
+            </Form.Item>
+          </Col>
+        </Row>
+        <Form.Item
+          noStyle
+          shouldUpdate={(perValues, nextValues) => {
+            return perValues['judgeWay'] !== nextValues['judgeWay'];
+          }}
+        >
+          {({ getFieldValue }) => {
+            const isVisible =
+              getFieldValue('judgeWay') &&
+              getFieldValue('judgeWay') === 'single';
+
+            return <>{isVisible ? <SingleDataConfig /> : <SubTaskConfig />}</>;
+          }}
+        </Form.Item>
+        <Divider />
+        <Form.Item>
+          <Button type={'primary'} htmlType="submit">
+            {t('Confirm_Import')}
+          </Button>
+        </Form.Item>
+      </Form>
+    </ConfigProviderWrapper>
   );
 };
 

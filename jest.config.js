886479162c53c6c3c8f0ad72fe888e5cf88f8b16
zhangjiajun <183728895@qq.com>
2023-11-30 17:02:53 +0800

    fix: 更改jest测试环境timezone

--- jest.config.js
+++ jest.config.js
@@ -148,7 +148,9 @@ const config = {
   testEnvironment: 'jsdom',
 
   // Options that will be passed to the testEnvironment
-  // testEnvironmentOptions: {},
+  testEnvironmentOptions: {
+    timezone: '+8',
+  },
 
   // Adds a location field to test results
   // testLocationInResults: false,

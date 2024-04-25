e520c0050b7ac3ce358d345247be96a992cffae7
RegMs If <me@regmsif.cf>
2024-04-25 16:12:05 +0800

    chore: update config

--- .dumirc.ts
+++ .dumirc.ts
@@ -7,7 +7,10 @@ export default defineConfig({
   themeConfig: {
     name: 'x-star-design',
     footer:
-      '<div class="dumi-default-footer">版权所有 杭州信友队教育科技有限公司 | Powered by <a href="https://d.umijs.org" target="_blank" rel="noreferrer">dumi</a></div>',
+      '<div class="dumi-default-footer">版权所有©杭州信友队教育科技有限公司 | Powered by <a href="https://d.umijs.org" target="_blank" rel="noreferrer">dumi</a></div>',
+    socialLinks: {
+      github: 'https://github.com/turingstar-tech/x-star-editor',
+    },
   },
   apiParser: {},
   resolve: {
--- src/types/image.d.ts
+++ src/types/image.d.ts
@@ -1,10 +1,11 @@
 declare module '*.svg' {
   export default string;
 }
+
 declare module '*.png' {
   export default string;
 }
+
 declare module '*.jpg' {
   export default string;
 }
-declare module 'randomcolor';

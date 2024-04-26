8281406f29420a6ac4d6b79dc25f81b3dda685e6
RegMs If <me@regmsif.cf>
2024-04-27 04:56:25 +0800

    feat: support virtual table summary

--- jest.config.js
+++ jest.config.js
@@ -90,7 +90,7 @@ const config = {
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   moduleNameMapper: {
-    '^antd/es/(.*)$': 'antd/lib/$1',
+    '^(.*)/es/(.*)$': '$1/lib/$2',
   },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
